8ba6a57da6b92e3e3d625fe91b08112d0843cb08
jongmin <jmlee@DESKTOP-HO02TQ1.localdomain>
2022-09-08 12:27:08 +0900

    Day 2 Practice

---
+++ Practice/app1.js
@@ -0,0 +1,25 @@
+
+console.log(5454545);
+console.log('lalalala');
+console.log("alalala");
+// 문자열 따옴표는 한가지로 통일
+
+console.log(5+2);
+console.log(5*2);
+console.log(5/2);
+
+// variable const = constant(상수)
+
+const a = 5;
+const b = 2;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+
+const myName = "jinn";
+
+// in js veryLongVariableName = 0;
+// python very_long_variable_name = 0;
+
+console.log("Hello I'm " + myName);
